5536bc87e09d5404e0a497aa6abea0384768f352
jiwnchoi <jiwnchoi@skku.edu>
2024-03-11 14:30:44 +0900

    chore(python): update dependencies, project name change

--- src/llm4vis/__init__.py
+++ src/llm4vis/__init__.py
@@ -2,6 +2,7 @@ import importlib.metadata
 import pathlib
 
 import anywidget
+import pandas as pd
 import traitlets
 
 try:
@@ -11,6 +12,14 @@ except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
 
-class Counter(anywidget.AnyWidget):
+class LLM4VIS(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "main.js"
     value = traitlets.Int(0).tag(sync=True)
+    api_key: str
+    df: pd.DataFrame
+
+    def __init__(self, api_key: str, df: pd.DataFrame, **kwargs):
+        super().__init__(**kwargs)
+        self.api_key = api_key
+        self.df = df
+        self.on_msg(self._handle_msg)
