69baa147f56602e92f818ee254ce61c629c42fee
jiwnchoi <jiwnchoi@skku.edu>
2024-04-16 13:15:53 +0900

    fix(widget): revert ChartView

--- src/widget/views/ChartView.tsx
+++ src/widget/views/ChartView.tsx
@@ -1,32 +1,22 @@
-import { Center, CenterProps, Flex, Image, Box } from "@chakra-ui/react";
+import { Center, CenterProps } from "@chakra-ui/react";
 import { useCharts, useColorMode } from "@hooks";
 import { Vega } from "react-vega";
 
 export default function ChartView(props: CenterProps & { chartSize: number }) {
   const { colorMode } = useColorMode();
-  const { currentChart, charts } = useCharts(props.chartSize);
+  const { currentChart } = useCharts(props.chartSize);
 
   return (
-    <Center flexDirection="column" {...props}>
+    <Center {...props}>
       {currentChart && (
         <Vega
           mode={"vega-lite"}
           spec={currentChart.spec}
           data={currentChart.data}
-          actions={true}
+          actions={false}
           theme={colorMode === "light" ? undefined : "dark"}
         />
       )}
-      <Flex overflowX="auto" width="full" maxWidth="300px" my={4}>
-        {charts.map((chart, index) => (
-          chart.thumbnail && (
-            <Box key={index} p={1} mr={0}>
-              <Image src={chart.thumbnail} alt={`Thumbnail ${index}`} boxSize="80px" />
-            </Box>
-          )
-        ))}
-      </Flex>
-
     </Center>
   );
 }
